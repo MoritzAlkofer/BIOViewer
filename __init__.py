913bf7db60e56a06aadf16c96d04f2291bdf7b28
Moritz Alkofer <moritz@Moritzs-MacBook-Air.fritz.box>
2025-01-03 13:20:17 +0100

    refactored code to improve seperation of concerns. wip

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from BIOViewer.viewer.viewer_event import EventViewer
-from BIOViewer.viewer.viewer_cont import ContinuousViewer
-from BIOViewer.utils.config import ContinuousConfig
-from BIOViewer.utils.config import EventConfig
---
+++ bioviewer/__init__.py
@@ -0,0 +1,4 @@
+from .viewer_cont import ContinuousViewer
+from .display import ContinuousDisplay
+# from .viewer_event import EventViewer
+# from .display import EventDisplay
