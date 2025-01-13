f20202cc431db8ea32e93c066cb93303918082a0
Moritz Alkofer <moritz@Moritzs-MacBook-Air.local>
2025-01-13 14:32:59 +0100

    cleaned um testing file

--- demos/testing.py
+++ demos/testing.py
@@ -18,10 +18,3 @@ signal1 = Signal(data=data,y_ticks=[1,2,3,4,5,6])
 # tie everything together in Coordinator
 viewer = Viewer(signals = [signal0,signal1])
 
-# make coordinator interactive
-viewer.fig.canvas.mpl_connect('key_press_event', lambda event: viewer.update(event.key))
-plt.ion()
-
-# show
-plt.show(block=True)
-
