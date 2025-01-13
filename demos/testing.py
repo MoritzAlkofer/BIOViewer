39d9cda583e5c9221f589e39ebbac1d1face8115
Moritz Alkofer <moritz@Moritzs-MacBook-Air.local>
2025-01-13 15:33:37 +0100

    added variable linewidth

--- demos/testing.py
+++ demos/testing.py
@@ -9,8 +9,9 @@ from bioviewer import Signal
 
 # init signals
 data = np.load('../demos/example.npy')
+data[:,128:2*128]=np.nan
 
-signal0 = Signal(data=data)
+signal0 = Signal(data=data,colors='bbbkrk',linewidth=1.3)
 signal1 = Signal(data=data)
 
 
