de7b999e0fc8818240b0120d83c0e58ec4a4cb3e
Moritz Alkofer <moritz@Moritzs-MacBook-Air.local>
2025-01-14 12:35:47 +0100

    implemented sharey and removed individual timestamp management on display level

--- demos/testing.py
+++ demos/testing.py
@@ -10,17 +10,20 @@ from bioviewer import Signal
 # init signals
 data = np.load('../demos/example.npy')
 
-data1 = data.copy()
-data1[:,:]=0
-marker = np.full((data.shape[0],data.shape[1]), 1.).copy()
-marker[:,:100] = np.nan
-marker[:,150:250] = np.nan
+timestamps = [3,7,12,21] #seconds
+markersize = 1 #second
+fs = 128
+marker = np.full((1,data.shape[1]), np.nan).copy()
+for timestamp in timestamps:
+    start = int((timestamp-markersize/2)*fs)
+    end = int((timestamp+markersize/2)*fs)
+    marker[:,start:end]=0
 
 # Example: Assign specific rows
 
-signal0 = Signal(data=data,fs=128,scale_factor=220,y_ticks=['a','b','c','d','e','f'],real_time=True,unit='mv',t_label='Time [h:m:s]',colors='bbbkbr',show_t_ticks=True,linewidth=1,show_scale=True)
+marker = Signal(data=marker,fs=128,show_scale=False,colors='r',linewidth=3,y_ticks=['events'])
+signal0 = Signal(data=data,fs=128,scale_factor=220,y_ticks=['a','b','c','d','e','f'],unit='mv',colors='bbbkbr',linewidth=1,show_scale=True)
 signal1 = Signal(data=data,fs=128)
 
 # tie everything together in Coordinator
-viewer = Viewer(signals = [signal0,signal1],figsize=(14,4))
-
+viewer = Viewer(signals = [marker,signal0,signal1],figsize=(14,4),timestamps=timestamps,stepsize=0.5)
