4a64252193caceb0719a6e1e8967428949c9bfd5
Moritz Alkofer <moritz@Moritzs-MacBook-Air.local>
2025-01-14 11:32:55 +0100

    added scale bar to plot

--- demos/testing.py
+++ demos/testing.py
@@ -9,12 +9,17 @@ from bioviewer import Signal
 
 # init signals
 data = np.load('../demos/example.npy')
-data[:,128:2*128]=np.nan
 
-signal0 = Signal(data=data,colors='bbbkrk',linewidth=1.3)
-signal1 = Signal(data=data)
+data1 = data.copy()
+data1[:,:]=0
+marker = np.full((data.shape[0],data.shape[1]), 1.).copy()
+marker[:,:100] = np.nan
+marker[:,150:250] = np.nan
 
+# Example: Assign specific rows
 
+signal0 = Signal(data=data,fs=128,scale_factor=220,y_ticks=['a','b','c','d','e','f'],real_time=True,unit='mv',t_label='Time [h:m:s]',colors='bbbkbr',show_t_ticks=True,linewidth=1,show_scale=True)
+signal1 = Signal(data=data,fs=128)
 
 # tie everything together in Coordinator
 viewer = Viewer(signals = [signal0,signal1])
